63971cddf52b68c5f7c44d273a386625995e8105
zap641 <zap641@hey.com>
2025-05-23 08:32:01 +1000

    added undo tree <leader>U

--- lua/plugins/zap641.lua
+++ lua/plugins/zap641.lua
@@ -43,4 +43,12 @@ return {
       )
     end,
   },
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>U", "<cmd>lua require('undotree').toggle()<cr>", desc = "Undo Tree" },
+    },
+  },
 }

9982a638070e5a1cf97a13933e6b263c60e98452
zap641 <zap641@hey.com>
2025-05-23 07:04:10 +1000

    Added files to github

---
+++ lua/plugins/zap641.lua
@@ -0,0 +1,31 @@
+return {
+  -- Change explorer <leader>e keymap to minifiles
+  {
+    "echasnovski/mini.files",
+    keys = {
+      {
+        "<leader>fm",
+        function()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open mini.files (Directory of Current File)",
+      },
+      {
+        "<leader>fM",
+        function()
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Open mini.files (cwd)",
+      },
+      {
+        "<leader><space>",
+        function()
+          require("mini.files").open(LazyVim.root(), true)
+        end,
+        desc = "Open mini.files (root)",
+      },
+      { "<leader>e", "<leader>fm", desc = "Open mini.files (Directory of Current File)", remap = true },
+      { "<leader>E", "<leader>fM", desc = "Open mini.files (cwd)", remap = true },
+    },
+  },
+}

154029b129bbbccb9960b7c02b0532632dc8e55b
zap641 <zap641@hey.com>
2025-05-23 07:56:32 +1000

    added select session to dashboard

--- lua/plugins/zap641.lua
+++ lua/plugins/zap641.lua
@@ -33,4 +33,14 @@ return {
       },
     },
   },
+  {
+    "snacks.nvim",
+    opts = function(_, opts)
+      table.insert(
+        opts.dashboard.preset.keys,
+        7,
+        { icon = "S", key = "S", desc = "Select Session", action = require("persistence").select }
+      )
+    end,
+  },
 }

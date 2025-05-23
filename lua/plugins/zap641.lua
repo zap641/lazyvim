da30fbc9fa110a516f3738eefc6eb6fa2ffcd943
zap641 <zap641@hey.com>
2025-05-23 19:53:20 +1000

    simplified mini.files

--- lua/plugins/zap641.lua
+++ lua/plugins/zap641.lua
@@ -4,28 +4,12 @@ return {
     "echasnovski/mini.files",
     keys = {
       {
-        "<leader>fm",
-        function()
-          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-        end,
-        desc = "Open mini.files (Directory of Current File)",
-      },
-      {
-        "<leader>fM",
-        function()
-          require("mini.files").open(vim.uv.cwd(), true)
-        end,
-        desc = "Open mini.files (cwd)",
-      },
-      {
         "<leader><space>",
         function()
           require("mini.files").open(LazyVim.root(), true)
         end,
         desc = "Open mini.files (root)",
       },
-      { "<leader>e", "<leader>fm", desc = "Open mini.files (Directory of Current File)", remap = true },
-      { "<leader>E", "<leader>fM", desc = "Open mini.files (cwd)", remap = true },
     },
     opts = {
       options = {
